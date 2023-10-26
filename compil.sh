c1a819c7cd6f0647633e25cff5350bd253c37fd5
Paul GRANDAMME <paul.grandamme@univ-st-etienne.fr>
2023-10-26 10:55:05 -0500

    add css style

--- compil.sh
+++ compil.sh
@@ -1,4 +1,4 @@
 pandoc research.md -f markdown -t html -s -o research.html
 
-pandoc index.md -f markdown -t html -s -o index.html
+pandoc index.md -f markdown -t html -s -o index.html --css index.css
 

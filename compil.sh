154b6dc4120def80a4ef3c2df18b7d693c97a82a
Paul GRANDAMME <paul.grandamme@univ-st-etienne.fr>
2023-11-16 10:21:36 +0100

    update CV

--- compil.sh
+++ compil.sh
@@ -1,6 +1,6 @@
 pandoc research.md -f markdown -t html -s -o research.html --css pandoc.css
 
-pandoc CV.md -f markdown -t html -o CV.html -css pandoc.css
+pandoc CV.md -f markdown -t html -s -o CV.html -css pandoc.css
 
 pandoc index.md -f markdown -t html -s -o index.html --css pandoc.css
 
